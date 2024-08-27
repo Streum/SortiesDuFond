e2d38e9fe88728e1e8b59e87e6843207ff13c6a7
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-08-27 16:14:10 +0200

    Ajout CRUD User

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/participant')]
+class ParticipantController extends AbstractController
+{
+    #[Route('/', name: 'app_participant_index', methods: ['GET'])]
+    public function index(ParticipantRepository $participantRepository): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'participants' => $participantRepository->findAll(),
+        ]);
+    }
+    #[Route('/me', name: 'app_user', methods: ['GET', 'POST'])]
+    public function infoProfile(Security $security): Response
+    {
+        // Récupérer l'utilisateur connecté
+        $participant = $this->getUser();
+
+        // Vérifier que l'utilisateur est connecté
+        if (!$participant) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+
+        // Passer l'utilisateur au template
+        return $this->render('participant/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+    #[Route('/new', name: 'app_participant_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('participant/new.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_participant_show', methods: ['GET'])]
+    public function show(Participant $participant): Response
+    {
+        return $this->render('participant/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_participant_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('participant/edit.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_participant_delete', methods: ['POST'])]
+    public function delete(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$participant->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($participant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use App\Entity\Sites;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+        'label' => 'Email',
+        'attr' => ['class' => 'form-control']
+    ])
+            ->add('pseudo', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password',
+                    'class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('site', EntityType::class, [
+                'class' => Sites::class,
+                'choice_label' => 'nomSite',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
