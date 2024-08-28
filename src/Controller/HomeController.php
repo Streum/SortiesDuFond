be769c2b4ca05cf798d33c13eb46947a570d05a5
lucasdev44 <168409947+lucasdev44@users.noreply.github.com>
2024-08-28 16:50:26 +0200

    gestion ville crud et lieux crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\SortiesRepository;
+use App\Repository\VillesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(SortiesRepository $sortiesRepository): Response
+    public function index(SortiesRepository $sortiesRepository, VillesRepository $villesRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'sorties' => $sortiesRepository->findAll(),
+            'villes' => $villesRepository->findAll(),
+
         ]);
     }#[Route('/administration', name: 'app_admin')]
     public function administration(): Response
