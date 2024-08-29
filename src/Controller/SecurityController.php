d51702b735fccefc744cbb7de804f3555e110cbd
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-08-29 12:37:31 +0200

    Ajout d'une redirection vers le login aprés un logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // Vérifie si l'utilisateur est déjà authentifié
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home'); // Redirige vers la page d'accueil s'il est connecté
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -25,8 +31,8 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->render('security/login.html.twig');
     }
 }
