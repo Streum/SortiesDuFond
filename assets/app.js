e174251329ea9859fe6287d32b9490415f6ff046
Gwendal Kerbastard <gwen.kerbastard@gmail.com>
2024-09-02 21:42:27 +0300

    Checkbox inscrit/PasInscrit disabled si l'autre est cochée

--- assets/app.js
+++ assets/app.js
@@ -3,10 +3,28 @@ import './vendor/bootstrap/bootstrap.index.js';
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.js';
 
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+
 import './styles/app.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox1 = document.getElementById('search_isInscrit');
+    var checkbox2 = document.getElementById('search_isNotInscrit');
+
+    if (checkbox1 && checkbox2) {
+        checkbox1.addEventListener('change', function() {
+            if (this.checked) {
+                checkbox2.disabled = true;
+            } else {
+                checkbox2.disabled = false;
+            }
+        });
+
+        checkbox2.addEventListener('change', function() {
+            if (this.checked) {
+                checkbox1.disabled = true;
+            } else {
+                checkbox1.disabled = false;
+            }
+        });
+    }
+});
