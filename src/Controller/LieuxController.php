31e98f469c3a1a58cb73c1d4bc983e4cc9d0aaea
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-09-04 16:48:28 +0200

    photos

--- src/Controller/LieuxController.php
+++ src/Controller/LieuxController.php
@@ -105,7 +105,7 @@ class LieuxController extends AbstractController
                 'name' => $lieu->getNomLieu(),
             ]), Response::HTTP_OK);
         }
-        
+
 
         return $this->render('lieux/edit_ajax.html.twig', [
             'form' => $form
